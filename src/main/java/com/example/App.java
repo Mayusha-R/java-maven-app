ca145180e78b753631f2b6cd2549b9f13819d762
Myusha-R <rathodmayusha@gmail.com>
2024-10-29 12:46:44 +0530

    git webhook trial 4

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, This was triggered by git-webhook 3");
+        System.out.println("Hello, This was triggered by git-webhook 4");
     }
 }
